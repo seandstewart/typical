4d3bb1812863d2f9c76c14a5a9e4d61689ac6fe9
Sean Stewart <sean_stewart@me.com>
2024-02-27 19:12:53 -0500

    Fix path types for <3.12

--- src/typical/types/path.py
+++ src/typical/types/path.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import pathlib
+import sys
+from typing import TYPE_CHECKING
 
 __all__ = ("FilePathError", "FilePath", "DirectoryPathError", "DirectoryPath")
 
@@ -14,33 +16,62 @@ class FilePathError(ValueError):
     pass
 
 
-class FilePath(PathType):  # type: ignore
-    """A path object pointing to a file.
-
-    See Also:
-        - :py:class:`pathlib.Path`
-    """
-
-    def __init__(self, *pathsegments: str):
-        super().__init__(*pathsegments)
-        if not self.is_file():
-            raise FilePathError(f"{self} is not a valid file-path") from None
-
-
 class DirectoryPathError(ValueError):
     """Generic error raised when the given value is not a Directory-Path."""
 
     pass
 
 
-class DirectoryPath(PathType):  # type: ignore
-    """A path object pointing to a directory.
+if TYPE_CHECKING:
 
-    See Also:
-        - :py:class:`pathlib.Path`
-    """
+    class FilePath(pathlib.Path): ...
 
-    def __init__(self, *pathsegments: str):
-        super().__init__(*pathsegments)
-        if not self.is_dir():
-            raise DirectoryPathError(f"{self} is not a valid directory-path") from None
+    class DirectoryPath(pathlib.Path): ...
+
+else:
+
+    class FilePath(PathType):  # type: ignore
+        """A path object pointing to a file.
+
+        See Also:
+            - :py:class:`pathlib.Path`
+        """
+
+        if sys.version_info >= (3, 12):
+
+            def __init__(self, *pathsegments: str):
+                super().__init__(*pathsegments)
+                if not self.is_file():
+                    raise FilePathError(f"{self} is not a valid file-path") from None
+
+        else:
+
+            def __init__(self, *pathsegments: str):
+                super().__init__()
+                if not self.is_file():
+                    raise FilePathError(f"{self} is not a valid file-path") from None
+
+    class DirectoryPath(PathType):  # type: ignore
+        """A path object pointing to a directory.
+
+        See Also:
+            - :py:class:`pathlib.Path`
+        """
+
+        if sys.version_info >= (3, 12):
+
+            def __init__(self, *pathsegments: str):
+                super().__init__(*pathsegments)
+                if not self.is_dir():
+                    raise DirectoryPathError(
+                        f"{self} is not a valid directory-path"
+                    ) from None
+
+        else:
+
+            def __init__(self, *pathsegments: str):
+                super().__init__()
+                if not self.is_dir():
+                    raise DirectoryPathError(
+                        f"{self} is not a valid directory-path"
+                    ) from None
